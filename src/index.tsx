1ad8330cce93e5ae88168abc2fa4175633a92637
Alex Bechmann <alex.bechmann@outlook.com>
2019-07-01 08:42:15 +0200

    unregister service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import { createMuiTheme } from "@material-ui/core";
-import { ThemeProvider } from "@material-ui/styles";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { createMuiTheme } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: "#25303b"
+      main: '#25303b'
     },
     secondary: {
-      main: "#ffb238"
+      main: '#ffb238'
     }
   }
 });
@@ -22,9 +22,9 @@ const Root = () => (
   </ThemeProvider>
 );
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.unregister();
