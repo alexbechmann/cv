735d1caaecb274af152535e42ac05ef038c176ae
Alex Bechmann <alex.bechmann@outlook.com>
2020-03-01 22:10:17 +0100

    build

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -12,7 +12,10 @@ const kubeconfig = sharedStack.getOutput("kubeconfig");
 const provider = new k8s.Provider("shared-cluster", {
   kubeconfig
 });
-const env = pulumi.getStack().replace("alexbechmanncv-", ""); //.split("-")[0];
+const env = pulumi.getStack();
+// .split("-")
+// .filter((_, i) => i !== 0)
+// .join("-");
 const project = "cv";
 const publicIp = "100.64.81.158";
 
