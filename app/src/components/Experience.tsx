cd46759dd45d4ce7ccaf0a8c82935e0ff516ac3e
Alex Bechmann <alex.bechmann@outlook.com>
2020-03-02 20:43:10 +0100

    add tag

--- app/src/components/Experience.tsx
+++ app/src/components/Experience.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Theme, makeStyles, Grid, Container, Slide } from '@material-ui/core';
-import JobCard, { JobCardProps } from './JobCard';
-import ExperienceDivider from './ExperienceDivider';
+import React from "react";
+import { Theme, makeStyles, Grid, Container, Slide } from "@material-ui/core";
+import JobCard, { JobCardProps } from "./JobCard";
+import ExperienceDivider from "./ExperienceDivider";
 
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
@@ -11,65 +11,77 @@ const useStyles = makeStyles((theme: Theme) => ({
 
 const jobs: JobCardProps[] = [
   {
-    companyName: 'Danfoss',
-    role: 'Web & CMS Consultant',
+    companyName: "Danfoss",
+    role: "Web & CMS Consultant",
     description:
-      'My current role is a full-stack DevOps developer, working on a number internal content management websites. With in a small team, we have built the company intranet site from the ground up, and i am actively involed in automation & cloud infrastructure for www.danfoss.com.',
-    timespan: 'Jan 2016 - Present',
-    color: '#e2000f',
-    url: 'https://www.danfoss.com/',
+      "My current role is a full-stack DevOps developer, working on a number internal content management websites. With in a small team, we have built the company intranet site from the ground up, and i am actively involed in automation & cloud infrastructure for www.danfoss.com.",
+    timespan: "Jan 2016 - Present",
+    color: "#e2000f",
+    url: "https://www.danfoss.com/",
     tasks: [],
     tags: [
-      'React',
-      'TypeScript',
-      'Azure DevOps',
-      'Umbraco',
-      'Docker',
-      'Azure',
-      'Code review',
-      'Scrum',
-      'Gitflow',
-      'MongoDB',
-      'NodeJS',
-      'GraphQL'
+      "React",
+      "TypeScript",
+      "Azure DevOps",
+      "Docker",
+      "Kubernetes",
+      "Azure",
+      "Code review",
+      "Scrum",
+      "Gitflow",
+      "MongoDB",
+      "NodeJS",
+      "GraphQL",
+      "Umbraco"
     ],
-    location: 'Nordborg, Denmark'
+    location: "Nordborg, Denmark"
   },
   {
-    companyName: 'Farlon',
-    role: '.NET Developer',
+    companyName: "Farlon",
+    role: ".NET Developer",
     description:
-      'My first job after graduating. I developed a web-app to facilitate importing, manipulation & exporting of customer data for their suite of Active Directory based call center solutions.',
-    timespan: 'Aug 2015 - Dec 2015',
-    color: '#005896',
-    url: 'https://www.farlon.com/',
+      "My first job after graduating. I developed a web-app to facilitate importing, manipulation & exporting of customer data for their suite of Active Directory based call center solutions.",
+    timespan: "Aug 2015 - Dec 2015",
+    color: "#005896",
+    url: "https://www.farlon.com/",
     tasks: [],
-    tags: ['.NET Framework', 'DevExpress', 'Solo project'],
-    location: 'Copenhagen, Denmark'
+    tags: [".NET Framework", "DevExpress", "Solo project"],
+    location: "Copenhagen, Denmark"
   },
   {
-    companyName: 'Ustwo',
-    role: 'iOS Developer intern',
+    companyName: "Ustwo",
+    role: "iOS Developer intern",
     description:
-      'I took part in an internship program with ustwo, a design & development agency in London. We learned how to thoroughly plan and validate project assumptions prior to using those skills to build native iOS apps for the main internship project and client work.',
-    timespan: 'Feb 2015 - Apr 2015',
-    color: '#ffbf02',
-    url: 'https://www.ustwo.com/',
+      "I took part in an internship program with ustwo, a design & development agency in London. We learned how to thoroughly plan and validate project assumptions prior to using those skills to build native iOS apps for the main internship project and client work.",
+    timespan: "Feb 2015 - Apr 2015",
+    color: "#ffbf02",
+    url: "https://www.ustwo.com/",
     tasks: [],
-    tags: ['Swift', 'PHP', 'Scrum', 'Git'],
-    location: 'London, United Kingdom'
+    tags: ["Swift", "PHP", "Scrum", "Git"],
+    location: "London, United Kingdom"
   },
   {
-    companyName: 'Business Academy Aarhus',
-    role: 'Student',
+    companyName: "Business Academy Aarhus",
+    role: "Student",
     description:
-      'I studied first Multimedia Design & Communication, before completing my Bachelors degree in Web Development at BAA. I also won a graduation award for the highest exam grades!',
-    timespan: 'Jan 2012 - June 2015',
-    color: '#57B7C9',
-    url: 'https://www.baaa.dk/',
+      "I studied first Multimedia Design & Communication, before completing my Bachelors degree in Web Development at BAA. I also won a graduation award for the highest exam grades!",
+    timespan: "Jan 2012 - June 2015",
+    color: "#57B7C9",
+    url: "https://www.baaa.dk/",
     tasks: [],
-    tags: ['.NET Framework', 'PHP', 'C#', 'Objective C', 'Photoshop', 'SQL', 'NoSQL', 'HTML', 'CSS', 'JavaScript'],
-    location: 'Aarhus, Denmark'
+    tags: [
+      ".NET Framework",
+      "PHP",
+      "C#",
+      "Objective C",
+      "Photoshop",
+      "SQL",
+      "NoSQL",
+      "HTML",
+      "CSS",
+      "JavaScript"
+    ],
+    location: "Aarhus, Denmark"
   }
 ];
 
@@ -80,13 +92,21 @@ const Experience = () => {
       <Grid container className={classes.root}>
         {jobs
           .map((job, index) => (
-            <Slide key={job.companyName} in={true} direction={index % 2 ? 'left' : 'right'}>
+            <Slide
+              key={job.companyName}
+              in={true}
+              direction={index % 2 ? "left" : "right"}
+            >
               <Grid xs={12} item>
                 <JobCard {...job} />
               </Grid>
             </Slide>
           ))
-          .reduce((acc, x) => (acc === null ? [x] : ([acc, <ExperienceDivider />, x] as any)), null)}
+          .reduce(
+            (acc, x) =>
+              acc === null ? [x] : ([acc, <ExperienceDivider />, x] as any),
+            null
+          )}
       </Grid>
     </Container>
   );
